094aac7b92cdc7f3da18b7054289ef497d9c63c6
NicolasDuboisToulouse <NicolasDuboisToulouse@users.noreply.github.com>
2019-08-02 11:51:01 +0200

    * Add tags accessors
    * Add save function

--- lib/PhotosNorm/ImageTag.pm
+++ lib/PhotosNorm/ImageTag.pm
@@ -8,6 +8,21 @@ package PhotosNorm::ImageTag;
 use strict;
 use warnings;
 use Image::ExifTool;
+use POSIX qw(mktime);
+
+
+#
+# Tags properties.
+#
+use constant {
+    TAG_NONE       => 0,
+    TAG_DIMENSIONS => 1,
+    TAG_DATE       => 2,
+    TAG_TITLE      => 4,
+    TAG_COMMENT    => 8,
+    TAG_CROP       => 16,
+};
+
 
 #
 # Create a new PhotosNorm::ImageTag from $file.
@@ -17,18 +32,227 @@ sub new
 {
     my ($pkg, $file) = @_;
 
+    my $self = bless( { file => $file }, $pkg );
+
+    return undef if (!$self->_read_exif_tags());
+
+    return $self;
+}
+
+#
+# Accessors. Some are r/w other ro.
+# Most of accessors may return undef.
+# No checks is done on setters.
+#
+
+# rw
+sub title
+{
+    $_[0]->{title} = $_[1] if (@_ == 2);
+    return $_[0]->{title};
+}
+
+# rw
+sub comment
+{
+    $_[0]->{comment} = $_[1] if (@_ == 2);
+    return $_[0]->{comment};
+}
+
+# rw
+sub date
+{
+    $_[0]->{date} = $_[1] if (@_ == 2);
+    return $_[0]->{date};
+}
+
+# rw
+# get/set date field with an unix timestamp
+sub timestamp
+{
+    my ($self, $timestamp) = @_;
+    if (defined($timestamp)) {
+        my ($sec, $min, $hour, $mday, $mon, $year) = localtime($timestamp);
+        $self->date(sprintf("%d:%02d:%02d %02d:%02d:%02d",
+                            $year + 1900, $mon + 1, $mday, $hour, $min, $sec));
+    }
+    my ($year, $mon, $day, $hour, $min, $sec) = split(/[:\/ ]/, $self->date());
+    return mktime($sec, $min, $hour, $day, $mon - 1, $year - 1900, 0, 0);
+}
+
+# ro
+# never undef.
+sub width
+{
+    return $_[0]->{width};
+}
+
+# ro
+# never undef.
+sub height
+{
+    return $_[0]->{height};
+}
+
+# ro
+# return hash { camera, iso, flash, exposure, focal, exposure_bias, aperture }
+# Nothing can be undef ('unkwown' by default)
+sub camera
+{
+    return $_[0]->{camera_infos};
+}
+
+
+# rw
+# return crop informations to create thumbnail.
+sub crop_left
+{
+    $_[0]->{crop}->{left} = $_[1] if (@_ == 2);
+    return $_[0]->{crop}->{left};
+}
+sub crop_top
+{
+    $_[0]->{crop}->{top} = $_[1] if (@_ == 2);
+    return $_[0]->{crop}->{top};
+}
+sub crop_width
+{
+    $_[0]->{crop}->{width} = $_[1] if (@_ == 2);
+    return $_[0]->{crop}->{width};
+}
+sub crop_height
+{
+    $_[0]->{crop}->{height} = $_[1] if (@_ == 2);
+    return $_[0]->{crop}->{height};
+}
+
+
+#
+# Save modified tags.
+# On success, return a bitset of TAG_* constants.
+# On failure, return undef.
+#
+# $taget_file is optional.
+# if defined :
+#  - The $target_file shall not exists
+#  - The $target_file content will be copied from file used to load tags,
+#  - Current tags will not be modified (a new call to save() will return the same bitset)
+# else :
+#  - Tags will be writed to file used to load tags.
+#  - Tags will be re-loaded (a new call to save() will return 0)
+#
+#
+sub save
+{
+    my ($self, $target_file) = @_;
+
+    my $updated_tags = TAG_NONE;
+
+    # Update exif dimensions if they are invalid
+    if (! defined($self->{exif_tools}->GetValue('ExifImageWidth')) ||
+        $self->{exif_tools}->GetValue('ExifImageWidth') ne $self->{width}) {
+        $self->{exif_tools}->SetNewValue('ExifImageWidth' => $self->{width});
+        $updated_tags |= TAG_DIMENSIONS;
+    }
+    if (! defined($self->{exif_tools}->GetValue('ExifImageHeight')) ||
+        $self->{exif_tools}->GetValue('ExifImageHeight') ne $self->{height}) {
+        $self->{exif_tools}->SetNewValue('ExifImageHeight' => $self->{height});
+        $updated_tags |= TAG_DIMENSIONS;
+    }
+
+    #Update date
+    if (defined($self->{date})) {
+        if (! defined($self->{exif_tools}->GetValue('DateTimeOriginal')) ||
+            $self->{exif_tools}->GetValue('DateTimeOriginal') ne $self->{date}) {
+            $self->{exif_tools}->SetNewValue('DateTimeOriginal' => $self->{date});
+            $updated_tags |= TAG_DATE;
+        }
+        # According to EXIF standard, this tag is the last modification date. So it should not be updated.
+        # But some tools use this tag instead of the DateTimeOriginal one.
+        if (! defined($self->{exif_tools}->GetValue('CreateDate')) ||
+            $self->{exif_tools}->GetValue('CreateDate') ne $self->{date}) {
+            $self->{exif_tools}->SetNewValue('CreateDate' => $self->{date});
+            $updated_tags |= TAG_DATE;
+        }
+    }
+
+    # Update title
+    if (defined($self->{title}) && (
+            ! defined($self->{exif_tools}->GetValue('ImageDescription')) ||
+            $self->{exif_tools}->GetValue('ImageDescription') ne $self->{title}
+        )) {
+        $self->{exif_tools}->SetNewValue('ImageDescription' => $self->{title});
+        $updated_tags |= TAG_TITLE;
+    }
+
+    # Update comment
+    if (defined($self->{comment}) && (
+            ! defined($self->{exif_tools}->GetValue('UserComment')) ||
+            $self->{exif_tools}->GetValue('UserComment') ne $self->{comment}
+        )) {
+        $self->{exif_tools}->SetNewValue('UserComment' => $self->{comment});
+        $updated_tags |= TAG_COMMENT;
+    }
+
+    #Update crop information
+    if (defined($self->{crop}->{left}) && (
+            ! defined($self->{exif_tools}->GetValue('XMP:CropLeft')) ||
+            $self->{exif_tools}->GetValue('XMP:CropLeft') ne $self->{crop}->{left}
+        )) {
+        $self->{exif_tools}->SetNewValue('XMP:CropLeft' => $self->{crop}->{left});
+        $updated_tags |= TAG_CROP;
+    }
+    if (defined($self->{crop}->{top}) && (
+            ! defined($self->{exif_tools}->GetValue('XMP:CropTop')) ||
+            $self->{exif_tools}->GetValue('XMP:CropTop') ne $self->{crop}->{top}
+        )) {
+        $self->{exif_tools}->SetNewValue('XMP:CropTop' => $self->{crop}->{top});
+        $updated_tags |= TAG_CROP;
+    }
+    if (defined($self->{crop}->{width}) && (
+            ! defined($self->{exif_tools}->GetValue('XMP:CropWidth')) ||
+            $self->{exif_tools}->GetValue('XMP:CropWidth') ne $self->{crop}->{width}
+        )) {
+        $self->{exif_tools}->SetNewValue('XMP:CropWidth' => $self->{crop}->{width});
+        $updated_tags |= TAG_CROP;
+    }
+    if (defined($self->{crop}->{height}) && (
+            ! defined($self->{exif_tools}->GetValue('XMP:CropHeight')) ||
+            $self->{exif_tools}->GetValue('XMP:CropHeight') ne $self->{crop}->{height}
+        )) {
+        $self->{exif_tools}->SetNewValue('XMP:CropHeight' => $self->{crop}->{height});
+        $updated_tags |= TAG_CROP;
+    }
+
+
+    return undef if (! $self->{exif_tools}->WriteInfo($self->{file}, $target_file));
+    $self->_read_exif_tags() if (!$target_file);
+    
+    return $updated_tags;
+}
+
+
+#
+# Read tags. Return 0 on error.
+# This function will overwrite all readed tags.
+#
+sub _read_exif_tags
+{
+    my ($self) = @_;
+
+    # Load tags
     my $exif_tool = new Image::ExifTool;
     $exif_tool->Options(Unknown => 1, Charset => 'UTF8');
 
-    return undef if (!$exif_tool->ExtractInfo($file));
-    return undef if ($exif_tool->GetValue('MIMEType') !~ /^image/);
+    return 0 if (!$exif_tool->ExtractInfo($self->{file}));
+    return 0 if ($exif_tool->GetValue('MIMEType') !~ /^image/);
 
-    my $self = bless( { file => $file, exif_tools => $exif_tool }, $pkg);
-
+    $self->{exif_tools} = $exif_tool;
+        
     # Read dimension from file header (not from tag informations)
     $self->{width} = $self->_read_exif_tag_int('ImageWidth');
     $self->{height} = $self->_read_exif_tag_int('ImageHeight');
-    return undef if (!$self->{width} || !$self->{height});
+    return 0 if (!$self->{width} || !$self->{height});
 
     # Read date. Check several fields because some camera doesn't follow EXIF standard
     my $date = $self->_read_exif_tag('DateTimeOriginal');
@@ -45,10 +269,10 @@ sub new
     $self->{comment} = $self->_read_exif_tag('UserComment');
 
     # Crop information to build thumbnail (non-standard tags, specific to this tool)
-    $self->{crop}->{top} = $self->_read_exif_tag('CropTop');
-    $self->{crop}->{left} = $self->_read_exif_tag('CropLeft');
-    $self->{crop}->{width} = $self->_read_exif_tag('CropWidth');
-    $self->{crop}->{height} = $self->_read_exif_tag('CropHeight');
+    $self->{crop}->{left} = $self->_read_exif_tag('XMP:CropLeft');
+    $self->{crop}->{top} = $self->_read_exif_tag('XMP:CropTop');
+    $self->{crop}->{width} = $self->_read_exif_tag('XMP:CropWidth');
+    $self->{crop}->{height} = $self->_read_exif_tag('XMP:CropHeight');
 
     #Get camera description
     my $make = $self->_read_exif_tag('Make') || '';
@@ -97,15 +321,10 @@ sub new
     $self->{camera_infos}->{flash} = 
         $self->_read_exif_tag('Flash') ||
         'Unknown';  
-    
-    
-    return $self;
+
+    1;
 }
 
-
-# TODO accessors (take care of r/w or ro)
-
-# TODO Save ExifImageWidth, ExifImageHeight, DateTimeOriginal, CreateDate, ...
 
 
 #
